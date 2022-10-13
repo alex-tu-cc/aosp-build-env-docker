1a6e25d07cf5f9f95575e19be2135d2f39ff6baf
Alex Tu <chitarraXD@gmail.com>
2022-10-13 23:58:59 +0800

    reset entry script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,10 +21,10 @@ sed -i -e "s/^${DOCKER_USER}:\([^:]*\):[0-9]*:[0-9]*/${DOCKER_USER}:\1:${HOST_US
 sed -i -e "s/^${DOCKER_USER}:\([^:]*\):[0-9]*/${DOCKER_USER}:\1:${HOST_USER_GID}/"  /etc/group
 chown ${HOST_USER_ID}:${HOST_USER_GID} /home/${DOCKER_USER} -R
 
-# allow user to run sudo
-adduser ${DOCKER_USER} sudo
+## allow user to run sudo
+#adduser ${DOCKER_USER} sudo
 
 #change to /workdir after login
-echo "cd /workdir" >> /home/${DOCKER_USER}/.bashrc
+echo "cd /workdir" > /home/${DOCKER_USER}/.bashrc
 
-su - "${DOCKER_USER}"
+su - "${DOCKER_USER}" -c bash
